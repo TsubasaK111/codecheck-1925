4edc906e8c3872339514ed040054760b47213fe8
TsubasaK111 <Tsubasa.K111@gmail.com>
2016-05-03 19:48:03 +0900

    refactor: remove more set_trace() and 'try:' arguments

--- solvify.py
+++ solvify.py
@@ -25,7 +25,6 @@ def recursive_solve(cell, grid):
             if last_cell(cell):
                 print "I think we have a winner!!! :D"
                 print_grid(grid)
-                pdb.set_trace()
                 return grid
             else:
                 solved_grid = recursive_solve(next_cell(cell, grid), grid)
@@ -42,24 +41,20 @@ def recursive_solve(cell, grid):
 
 
 def print_grid(grid):
-    try:
-        print "#########"
-        for X, grid_row in enumerate(grid):
-            for x in range(0,3):
-                output = "["
-                for Y in range(0,3):
-                    output += "[ "
-                    for y in range(0,3):
-                        try:
-                            output += str(grid[X][Y][x][y].value)
-                        except:
-                            output += "*" + str(grid[X][Y][x][y])
-                    output += " ]"
-                output = output + "]"
-                print output
-    except: # catch *all* exceptions
-        error = sys.exc_info()[0]
-        pdb.set_trace()
+    print "#########"
+    for X, grid_row in enumerate(grid):
+        for x in range(0,3):
+            output = "["
+            for Y in range(0,3):
+                output += "[ "
+                for y in range(0,3):
+                    try:
+                        output += str(grid[X][Y][x][y].value)
+                    except:
+                        output += "*" + str(grid[X][Y][x][y])
+                output += " ]"
+            output = output + "]"
+            print output
 
 
 def save_cell(cell, grid):
@@ -82,32 +77,26 @@ def last_cell(cell):
 
 
 def next_cell(cell, grid):
-    try:
-        print "cell was: ", cell.__repr__()
-        X = cell.X
-        Y = cell.Y
-        x = cell.x
-        y = cell.y
-        y += 1
-        if y > 2:
-            y = 0
-            x += 1
-            if x > 2:
-                x = 0
-                Y += 1
-                if Y > 2:
-                    Y = 0
-                    X += 1
-                    if X > 2:
-                        pdb.set_trace()
-                        return cell
-        new_cell = grid[X][Y][x][y]
-        if new_cell.constant == True:
-            print "cell is constant!"
-            new_cell = next_cell(new_cell, grid)
-        print "next up: ", new_cell.__repr__()
-        return new_cell
-
-    except: # catch *all* exceptions
-        error = sys.exc_info()[0]
-        pdb.set_trace()
+    print "cell was: ", cell.__repr__()
+    X = cell.X
+    Y = cell.Y
+    x = cell.x
+    y = cell.y
+    y += 1
+    if y > 2:
+        y = 0
+        x += 1
+        if x > 2:
+            x = 0
+            Y += 1
+            if Y > 2:
+                Y = 0
+                X += 1
+                if X > 2:
+                    return cell
+    new_cell = grid[X][Y][x][y]
+    if new_cell.constant == True:
+        print "cell is constant!"
+        new_cell = next_cell(new_cell, grid)
+    print "next up: ", new_cell.__repr__()
+    return new_cell
--- sudoku.py
+++ sudoku.py
@@ -2,17 +2,15 @@ import pdb, pprint
 
 from matrixify import block_matrixify, un_block_matrixify
 from cellify import Cell, cellify, uncellify
-import solvify
+from solvify import solve_grid
 
 def solve(flat_array):
     print flat_array
     grid = block_matrixify(flat_array)
     grid = cellify(grid)
-    solved_grid = solvify.solve_grid(grid)
-    solvify.print_grid(solved_grid)
+    solved_grid = solve_grid(grid)
     grid = uncellify(grid)
     solved_flat_array = un_block_matrixify(grid)
     print flat_array
     print solved_flat_array
-    pdb.set_trace()
     return solved_flat_array
